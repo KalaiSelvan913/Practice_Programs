f338289256943672aab874a5b784a132897b0fce
KalaiSelvan913 <kalaiselvan4913@gmail.com>
2024-05-25 12:17:53 +0530

    added practice codes

---
+++ src/program/RamNod.java
@@ -0,0 +1,15 @@
+package program;
+
+public class RamNod {
+	
+	int val;
+	
+	RamNod next;
+	
+	RamNod prev;
+	
+	RamNod(int val){
+		this.val=val;
+	}
+
+}
---
+++ src/program/SeatFinder.java
@@ -0,0 +1,9 @@
+package program;
+
+public class SeatFinder {
+	
+	public static void main(String[] arg) {
+		System.out.println(1%2);
+	}
+
+}
